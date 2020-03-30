49f647f9db205b89dc94bd0745a2e5a1dae36ac1
Oscar <oscar_bss_crmn@hotmail.com>
2020-03-31 00:06:01 +0200

    Update Calculadora.php

--- Calculadora.php
+++ Calculadora.php
@@ -3,27 +3,26 @@
 class calculadora{
 
 public function operacion($num1,$num2,$opcion){
-
-switch($opcion){
-    case 'suma':
-	$resultado = $this->sumar($num1,$num2);
-        return $resultado;
-	break;
-    case 'resta':
-	$resultado = $this->restar($num1,$num2);
-	return $resultado;
-	break;
-    case 'multiplica':
-	$resultado = $this->multiplicar($num1,$num2);
-	return $resultado;
-	break;
-    case 'divide':
-	$resultado = $this->dividir($num1,$num2);
-	return $resultado;
-	break;
-     default:
-        break;
-}
+	switch($opcion){
+	    case 'suma':
+		$resultado = $this->sumar($num1,$num2);
+		return $resultado;
+		break;
+	    case 'resta':
+		$resultado = $this->restar($num1,$num2);
+		return $resultado;
+		break;
+	    case 'multiplica':
+		$resultado = $this->multiplicar($num1,$num2);
+		return $resultado;
+		break;
+	    case 'divide':
+		$resultado = $this->dividir($num1,$num2);
+		return $resultado;
+		break;
+	     default:
+		break;
+	}
 }
 
 public function sumar($num1,$num2){
